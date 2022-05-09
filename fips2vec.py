505162b1026df8119ebc4eaaea1fc946fec2964e
Rob Rossmiller <rrossmiller3@gmail.com>
2022-05-09 14:53:41 -0400

    update labels to have county and state names

--- fips2vec.py
+++ fips2vec.py
@@ -1,9 +1,13 @@
 def fips2vec(dimensions=64, force_train=False, vizualize=False, workers=6, file_name='fips2vec'):
     import os
+
     if not os.path.exists('fips2vec.kv') or force_train:
         import pandas as pd
         import networkx as nx
         from node2vec import Node2Vec
+        from tqdm import trange
+
+        print('loading raw fips file')
         # source: https://www2.census.gov/geo/docs/reference/county_adjacency.txt
         with open('fips_adjacency_raw.txt') as f:
             raw_zips = f.readlines()
@@ -11,19 +15,26 @@ def fips2vec(dimensions=64, force_train=False, vizualize=False, workers=6, file_
         # format the raw txt
         edges = []
         curr = ''
-        for i in range(len(raw_zips)):
+        names = []
+        for i in trange(len(raw_zips)):
             row = raw_zips[i].replace('\n', '')
             if row[0] == '"':
                 tmp = row.split('\t')
                 curr = tmp[1]
                 edges.append([curr, tmp[3]])
-
+                names.append((tmp[1], tmp[0].replace('"','')))
+                names.append((tmp[3], tmp[2].replace('"','')))
             else:
                 row = row.split('\t')
                 edges.append([curr, row[-1]])
-
+                names.append((row[-1], row[-2].replace('"','')))
+        
+        names_df = pd.DataFrame(names, columns=['num', 'name'])
+        names_df = names_df.drop_duplicates()
+        names_df.to_csv('names.csv', index=False) 
         edges_df = pd.DataFrame(edges, columns=['src', 'tgt'])
         edges_df.to_csv('fips_adjacency.csv', index=False) #save the graph as a csv
+        return
         g = nx.Graph()
         g.add_edges_from(edges_df.to_numpy())
 
@@ -44,31 +55,36 @@ def fips2vec(dimensions=64, force_train=False, vizualize=False, workers=6, file_
             viz.layout()  # layout with default (neato)
             viz.draw("graph.png")  # draw png
     else:
-			import gensim
-			embedding = gensim.models.KeyedVectors.load_word2vec_format('fips2vec.kv')
-			print(embedding['53045'])
+        import gensim
+        embedding = gensim.models.KeyedVectors.load_word2vec_format('fips2vec.kv')
+        print(embedding['53045'])
 
 def write_tsv(file_path='.'):
-	import csv
-	with open('fips2vec.kv') as fin:
-		file = fin.readlines()
+    import csv
+    import pandas as pd
 
-	file.pop(0)
-	vecs = []
-	labels = []
-	for i in range(len(file)):
-		splits = file[i].split()
-		labels.append(splits[0]+'\n')
-		vecs.append(splits[1:])
+    with open('fips2vec.kv') as fin:
+        file = fin.readlines()
 
-	with open(f'{file_path}/fips_vecs.tsv', 'w') as fout:
-		csv.writer(fout, delimiter='\t').writerows(vecs)
+    file.pop(0)
+    vecs = []
+    labels = ['FIPS\tName\n']
+    names_df = pd.read_csv('names.csv') 
+    for i in range(len(file)):
+        splits = file[i].split()
+        if len(names_df.loc[names_df.num == int(splits[0])].name) == 0:
+            print(splits)
+            print(names_df.loc[names_df.num == int(splits[0])].name)
+        labels.append(f'{splits[0]}\t{names_df.loc[names_df.num == int(splits[0])].name.iloc[0]}\n')
+        vecs.append(splits[1:])
 
-	print(labels[-1].encode())
-	labels[-1] = labels[-1].replace('\n','')
-	print(labels[-1].encode())
-	with open(f'{file_path}/fips_labels.tsv', 'w') as fout:
-		fout.writelines(labels)
+    with open(f'{file_path}/fips_vecs.tsv', 'w') as fout:
+        csv.writer(fout, delimiter='\t').writerows(vecs)
+
+    labels[-1] = labels[-1].replace('\n','')
+    print(labels[-1])
+    with open(f'{file_path}/fips_labels.tsv', 'w') as fout:
+        fout.writelines(labels)
 		
 if __name__ == '__main__':
   fips2vec(dimensions=200, force_train=True)

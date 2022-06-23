6b39e09f3774ba82b6cebf1068eb5182f2f723fc
Rob Rossmiller <rrossmiller3@gmail.com>
2022-06-23 17:30:15 -0400

    update and format

--- fips2vec.py
+++ fips2vec.py
@@ -1,100 +1,109 @@
-def fips2vec(dimensions=64, force_train=False, vizualize=False, workers=6, file_name='fips2vec'):
+def fips2vec(
+    dimensions, force_train=False, vizualize=False, workers=6, file_name="fips2vec"
+):
     import os
 
-    if not os.path.exists('fips2vec.kv') or force_train:
+    if not os.path.exists("fips2vec.kv") or force_train:
         import pandas as pd
         import networkx as nx
         from node2vec import Node2Vec
         from tqdm import trange
 
-        print('loading raw fips file')
+        print("loading raw fips file")
         # source: https://www2.census.gov/geo/docs/reference/county_adjacency.txt
-        with open('fips_adjacency_raw.txt') as f:
+        with open("fips_adjacency_raw.txt") as f:
             raw_zips = f.readlines()
 
         # format the raw txt
         edges = []
-        curr = ''
+        curr = ""
         names = []
         for i in trange(len(raw_zips)):
-            row = raw_zips[i].replace('\n', '')
+            row = raw_zips[i].replace("\n", "")
             if row[0] == '"':
-                tmp = row.split('\t')
+                tmp = row.split("\t")
                 curr = tmp[1]
                 edges.append([curr, tmp[3]])
-                names.append((tmp[1], tmp[0].replace('"','')))
-                names.append((tmp[3], tmp[2].replace('"','')))
+                names.append((tmp[1], tmp[0].replace('"', "")))
+                names.append((tmp[3], tmp[2].replace('"', "")))
             else:
-                row = row.split('\t')
+                row = row.split("\t")
                 edges.append([curr, row[-1]])
-                names.append((row[-1], row[-2].replace('"','')))
-        
-        names_df = pd.DataFrame(names, columns=['num', 'name'])
+                names.append((row[-1], row[-2].replace('"', "")))
+
+        names_df = pd.DataFrame(names, columns=["num", "name"])
         names_df = names_df.drop_duplicates()
-        names_df.to_csv('names.csv', index=False) 
-        edges_df = pd.DataFrame(edges, columns=['src', 'tgt'])
-        edges_df.to_csv('fips_adjacency.csv', index=False) #save the graph as a csv
+        names_df.to_csv("names.csv", index=False)
+        edges_df = pd.DataFrame(edges, columns=["src", "tgt"])
+        edges_df.to_csv("fips_adjacency.csv", index=False)  # save the graph as a csv
         g = nx.Graph()
         g.add_edges_from(edges_df.to_numpy())
 
-        node2vec = Node2Vec(g, dimensions=dimensions, walk_length=30, num_walks=200, workers=workers)
+        node2vec = Node2Vec(
+            g, dimensions=dimensions, walk_length=30, num_walks=200, workers=workers
+        )
 
         # Any keywords acceptable by gensim.Word2Vec can be passed, `dimensions` and `workers` are automatically passed (from the Node2Vec constructor)
-        model = node2vec.fit(window=10, min_count=1,
-                             batch_words=4)
+        model = node2vec.fit(window=10, min_count=1, batch_words=4)
 
         # Save embeddings for later use
-        if file_name:
-            model.wv.save_word2vec_format(file_name+'.kv')
+        model.wv.save_word2vec_format(file_name + ".kv")
 
         if vizualize:
-            print(model.wv['53045'])
-            print(model.wv.most_similar('53045'))
+            print(model.wv["55133"])
+            print(model.wv.most_similar("55133"))
             viz = nx.nx_agraph.to_agraph(g)
             viz.layout()  # layout with default (neato)
             viz.draw("graph.png")  # draw png
     else:
         import gensim
-        embedding = gensim.models.KeyedVectors.load_word2vec_format('fips2vec.kv')
-        print(embedding['53045'])
 
-def write_tsv(file_path='.'):
+        embedding = gensim.models.KeyedVectors.load_word2vec_format("fips2vec.kv")
+        print(embedding["55133"])
+
+
+def write_tsv(dimensions, file_path="."):
     import csv
     import pandas as pd
 
-    with open('fips2vec.kv') as fin:
+    with open("fips2vec.kv") as fin:
         file = fin.readlines()
 
     file.pop(0)
     vecs = []
-    labels = ['FIPS\tName\n']
-    names_df = pd.read_csv('names.csv') 
+    labels = ["FIPS\tName\n"]
+    names_df = pd.read_csv("names.csv")
     for i in range(len(file)):
         splits = file[i].split()
         if len(names_df.loc[names_df.num == int(splits[0])].name) == 0:
             print(splits)
             print(names_df.loc[names_df.num == int(splits[0])].name)
-        labels.append(f'{splits[0]}\t{names_df.loc[names_df.num == int(splits[0])].name.iloc[0]}\n')
+        labels.append(
+            f"{splits[0]}\t{names_df.loc[names_df.num == int(splits[0])].name.iloc[0]}\n"
+        )
         vecs.append(splits[1:])
 
-    with open(f'{file_path}/fips_vecs.tsv', 'w') as fout:
-        csv.writer(fout, delimiter='\t').writerows(vecs)
+    with open(f"{file_path}/fips_vecs-{dimensions}.tsv", "w") as fout:
+        csv.writer(fout, delimiter="\t").writerows(vecs)
 
-    labels[-1] = labels[-1].replace('\n','')
+    labels[-1] = labels[-1].replace("\n", "")
     print(labels[-1])
-    with open(f'{file_path}/fips_labels.tsv', 'w') as fout:
+    with open(f"{file_path}/fips_labels.tsv", "w") as fout:
         fout.writelines(labels)
-		
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     import argparse
     import os
+
     os.system("clear")
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--dims", default=8)
     parser.add_argument("--force_train", default=False)
     dims = parser.parse_args().dims
+    dims = int(dims)
     force_train = parser.parse_args().force_train
 
     fips2vec(dimensions=dims, force_train=force_train)
-    write_tsv()
+    write_tsv(dims)
